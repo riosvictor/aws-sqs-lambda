ede37ad3146045789dd707ef6513eb0dd4667d7c
Paulo Victor Rios <victorios.ba@gmail.com>
2024-04-19 14:40:36 -0300

    chore: update libs

--- src/producer.ts
+++ src/producer.ts
@@ -1,22 +1,22 @@
+import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
 import { APIGatewayProxyHandler, APIGatewayEvent, ProxyResult } from 'aws-lambda';
-import { SQS } from 'aws-sdk';
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayEvent): Promise<ProxyResult> => {
-  const sqs = new SQS({
+  const client = new SQSClient({
     endpoint: process.env.QUEUE_HOST_URL,
     region: process.env.AWS_REGION
   });
 
   const queueUrl = process.env.QUEUE_URL ?? ''
   const body = JSON.parse(event.body ?? '{}')
-  
-  const params: SQS.SendMessageRequest = {
+
+  const command = new SendMessageCommand({
     MessageBody: JSON.stringify(body),
     QueueUrl: queueUrl,
-  };
-
+  });
+  
   try {
-    const data = await sqs.sendMessage(params).promise();
+    const data = await client.send(command);
     
     return {
       statusCode: 200,
